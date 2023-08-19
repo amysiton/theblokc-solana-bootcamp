66a216db688d515ad0807c8f4a9f46786ad08d42
amysiton <amymariesiton@gmail.com>
2023-08-19 21:35:32 +0800

    Create config.js

---
+++ dapp/solana_react/config.js
@@ -0,0 +1,38 @@
+import { ProvidePlugin } from "webpack";
+
+module.exports = function (config, env) {
+    return {
+        ...config,
+        module: {
+            ...config.module,
+            rules: [
+                ...config.module.rules,
+                {
+                    test: /\.(m?js|ts)$/,
+                    enforce: "pre",
+                    use: ["source-map-loader"],
+                },
+            ],
+        },
+        plugins: [
+            ...config.plugins,
+            new ProvidePlugin({
+                process: "process/browser",
+            }),
+        ],
+        resolve: {
+            ...config.resolve,
+            fallback: {
+                assert: require.resolve("assert"),
+                buffer: require.resolve("buffer"),
+                stream: require.resolve("stream-browserify"),
+                https: require.resolve("https-browserify"),
+                http: require.resolve("stream-http"),
+                zlib: require.resolve("browserify-zlib"),
+                url: require.resolve("url/"), 
+                crypto: require.resolve("crypto-browserify"),
+            },
+        },
+        ignoreWarnings: [/Failed to parse source map/],
+    };
+};
